1f8c5d288f1f42ceafe9538eee881cf4b005915a
Max Shen <m4xshen@gmail.com>
2024-03-07 15:19:44 +0800

    feat: add lab 2

--- Lab2/multi_control.py
+++ Lab2/multi_control.py
@@ -1,61 +1,76 @@
 #
-# Tello Python3 Control Demo 
+# Tello Python3 Control Demo
 #
 # http://www.ryzerobotics.com/
 #
 # 1/1/2018
 
-import threading 
+import threading
 import socket
 import sys
 import time
 
 
-host = ''
+host = ""
 port = 9000
-locaddr = (host,port) 
+locaddr = (host, port)
 
 
 # Create a UDP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind(locaddr)
 
-#please fill UAV IP address
-tello_address1 = ('???', 8889)
-tello_address2 = ('???', 8889)
+# please fill UAV IP address
+tello_address1 = ("192.168.137.109", 8889)
+tello_address2 = ("192.168.137.211", 8889)
 
-message1=["command", "takeoff",  "forward 200", "land"]
-message2=["command", "takeoff",  "forward 200", "land"]
-delay=[3,8,8,2]
+message1 = [
+    "command",
+    "battery?",
+    "takeoff",
+
+    "forward 150",
+    "up 50",
+    "forward 120",
+    "stop",
+
+    "land",
+]
+
+message2 = [
+    "command",
+    "battery?",
+    "takeoff",
+
+    "up 130",
+    "forward 110",
+    "down 130",
+    "forward 170",
+
+    "land",
+]
+delay = [3, 5, 7, 5, 5, 5, 5, 5]
+
 
 def recv():
     count = 0
-    while True: 
+    while True:
         try:
             data, server = sock.recvfrom(1518)
-            print("{} : {}".format(server,data.decode(encoding="utf-8")))
+            print("{} : {}".format(server, data.decode(encoding="utf-8")))
         except Exception:
-            print ('\nExit . . .\n')
+            print("\nExit . . .\n")
             break
 
 
-print ('\r\n\r\nTello Python3 Demo.\r\n')
-
-print ('Tello: command takeoff land flip forward back left right \r\n       up down cw ccw speed speed?\r\n')
-
-print ('end -- quit demo.\r\n')
-
-
-#recvThread create
+# recvThread create
 recvThread = threading.Thread(target=recv)
 recvThread.start()
 
 
-for i in range(0,len(message1)):
-    msg1=message1[i]
-    msg2=message2[i]
+for i in range(0, len(message1)):
+    msg1 = message1[i]
+    msg2 = message2[i]
     sock.sendto(msg1.encode("utf-8"), tello_address1)
     sock.sendto(msg2.encode("utf-8"), tello_address2)
     time.sleep(delay[i])
-
-
--- Lab2/opencv_test.py
+++ Lab2/opencv_test.py
@@ -1,15 +1,56 @@
 # coding=utf-8
+import threading
 import cv2
 import time
+import socket
 
-cap=cv2.VideoCapture("udp://192.168.10.1:?")
+host = ""
+port = 9000
+locaddr = (host, port)
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind(locaddr)
 
+
+def recv():
+    while True:
+        try:
+            # 監聽此socket，當收到資料的時候就會執行 data, server = sock.recvfrom(1518)
+            # data為本機收到的資料，資料須先用utf-8解碼後才會變成字串
+            # server為無人機的IP
+            data, server = sock.recvfrom(1518)
+            print("{} : {}".format(server, data.decode(encoding="utf-8")))
+        except Exception:
+            print("\nExit . . .\n")
+            break
+
+
+recvThread = threading.Thread(target=recv)
+recvThread.start()
+print("start recv")
+
+messages = [
+    "command",
+    "battery?",
+    "streamon",
+]
+
+uavIp = "192.168.10.1"
+
+print("start commands")
+for i in range(0, len(messages)):
+    msg1 = messages[i]
+    sock.sendto(msg1.encode("utf-8"), (uavIp, 8889))
+    time.sleep(5)
+
+cap = cv2.VideoCapture("udp://" + uavIp + ":11111")
+
+print("start capture")
 while True:
-	isFrame, frame=cap.read()
-
-	if isFrame:
-		cv2.imshow("UAV video",frame)
-	if cv2.waitKey(1) & 0xFF == ord('q'):
-		break
+    isFrame, frame = cap.read()
+    print(isFrame)
+    if isFrame:
+        cv2.imshow("UAV video", frame)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
 cap.release()
 cv2.destroyAllWindows()
